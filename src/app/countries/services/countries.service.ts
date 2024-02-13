1391b4325a5a53af2a2f46d8dd91f8762968375a
Mdperez19 <mdperezpriego@gmail.com>
2024-02-12 22:49:11 -0600

    Update countries.service.ts with error handling and logging

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap, map, catchError,of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({
@@ -13,7 +13,18 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   searchCapital(term: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiURL}/capital/${term}`);
+    return this.http.get<Country[]>(`${this.apiURL}/capital/${term}`)
+      .pipe(
+
+        catchError(err => {
+          console.log('Error: ', err);
+          return of([])
+        }), // Manejo de errores, of es un observable que emite un valor y luego se completa
+        // Ejemplo de uso de tap y map
+        /* tap(countries => console.log('paso por el tap ', countries)),
+        map(countries => []),
+        tap(countries => console.log('paso por el segundo tap ', countries)) */
+      );
   }
 
 }
