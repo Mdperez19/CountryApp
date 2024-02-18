5cb5965084199b024ad6462be4d2dbad75767561
Mdperez19 <mdperezpriego@gmail.com>
2024-02-17 23:19:13 -0600

    Refactor search methods in CountriesService

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -12,25 +12,13 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
-  searchCountryByAlphaCode(code: string): Observable<Country | null>{
-    return this.http.get<Country[]>(`${this.apiURL}/alpha/${code}`)
+  private getCountriesRequest(url: string): Observable<Country[]>{
+    return this.http.get<Country[]>(url)
       .pipe(
-        map((countries: Country[]) => countries.length > 0 ? countries[0] : null),
-        catchError(err => {
-
-          return of(null)
-        }),
-      );
-  }
-
-  searchCapital(term: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiURL}/capital/${term}`)
-      .pipe(
-
         catchError(err => {
           console.log('Error: ', err);
           return of([])
-        }), // Manejo de errores, of es un observable que emite un valor y luego se completa
+        }),// Manejo de errores, of es un observable que emite un valor y luego se completa
         // Ejemplo de uso de tap y map
         /* tap(countries => console.log('paso por el tap ', countries)),
         map(countries => []),
@@ -38,26 +26,29 @@ export class CountriesService {
       );
   }
 
-  searchCountry(term: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiURL}/name/${term}`)
+  searchCountryByAlphaCode(code: string): Observable<Country | null>{
+    return this.http.get<Country[]>(`${this.apiURL}/alpha/${code}`)
       .pipe(
-
+        map((countries: Country[]) => countries.length > 0 ? countries[0] : null),
         catchError(err => {
-          console.log('Error: ', err);
-          return of([])
+          return of(null)
         }),
       );
   }
 
-  searchRegion(region: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiURL}/region/${region}`)
-      .pipe(
+  searchCapital(term: string): Observable<Country[]>{
+    const url = `${this.apiURL}/capital/${term}`;
+    return this.getCountriesRequest(url);
+  }
 
-        catchError(err => {
-          console.log('Error: ', err);
-          return of([])
-        }),
-      );
+  searchCountry(term: string): Observable<Country[]>{
+    const url = `${this.apiURL}/name/${term}`;
+    return this.getCountriesRequest(url);
+  }
+
+  searchRegion(region: string): Observable<Country[]>{
+    const url = `${this.apiURL}/region/${region}`;
+    return this.getCountriesRequest(url);
   }
 
 }
